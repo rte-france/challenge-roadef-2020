c92a38c9fe5331b61b252c912e31c133a8f20d56
Manuel Ruiz <manuel.ruiz@rte-france.com>
2020-03-12 16:41:18 +0100

    Add readme and checker sources

---
+++ README.md
@@ -0,0 +1,321 @@
+# 2020 ROADEF - RTE : Solution checker user guide
+
+## General notice
+---
+The solution cheker is developped in Python3.
+
+It was not designed to be efficient, but rather to be easily understandable.
+
+To ease its usage, the whole source code is embedded in a single file, which shall be lauched as a script.
+
+
+## Usage
+---
+The `RTE_ChallengeROADEF2020_checker` script accepts 2 arguments:
+- *instance_file* : path to the instance file, in `.json` format
+- *solution_file* : path to corresponding solution file, in `.txt` format
+
+**Remark:** some of the instance files are very large (several gigas), reading them might take a few minutes.
+
+### Example
+```
+python RTE_ChallengeROADEF2020_checker.py exemple1.json output1.txt
+```
+
+## Produced output
+---
+The script prompts all detected inconsistencies on standard output, and computes a few indicators to evaluate the solution overall quality.
+
+The solution is considered valid if no error has been detected.
+
+
+### Successfull checking output example
+```
+Reading json file example1.json...
+Done
+Loading solution from output1.txt...
+Done
+Checking constraints...
+        Checking schedule constraints...
+        Done
+        Checking resources constraints...
+        Done
+        Checking exclusions constraints...
+        Done
+Done
+Computing objectives values...
+        Computing risk...
+        Done
+        Computing mean risk...
+        Done
+        Computing Q95...
+        Done
+Done
+Instance infos:
+        Interventions number:  3
+        Scenario numbers:  3
+Solution evaluation:
+        mean_risk over time:  [4.66666667 4.         3.        ]
+        Objective 1 (mean risk):  3.8888888888888893
+        Q95 over time:  [5. 8. 4.]
+        Objective 2 (expected excess):  1.7777777777777777
+```
+
+### Failed checking output example
+```
+Reading json file example1.json...
+Done
+Loading solution from output1.txt...
+ERROR: Unexpected Intervention I4 in solution file../resources/output/output1.txt.
+Done
+Checking constraints...
+        Checking schedule constraints...
+ERROR: Schedule constraint 4.1.2: Intervention I1 has not been scheduled.
+        Done
+        Checking resources constraints...
+ERROR: Resources constraint 4.2 lower bound: Workload on Resource c1 at time 0 does not match lower bound. Value 0.0 is lower than bound 10 minus tolerance 1e-05.
+ERROR: Resources constraint 4.2 lower bound: Workload on Resource c1 at time 2 does not match lower bound. Value 0.0 is lower than bound 6 minus tolerance 1e-05.
+        Done
+        Checking exclusions constraints...
+ERROR: Exclusions constraint 4.3: Interventions I2 and I3 are both ongoing at time 2.
+        Done
+Done
+Computing objectives values...
+        Computing risk...
+        Done
+        Computing mean risk...
+        Done
+        Computing Q95...
+        Done
+Done
+Instance infos:
+        Interventions number:  3
+        Scenario numbers:  3
+Solution evaluation:
+        mean_risk over time:  [0. 4. 0.]
+        Objective 1 (mean risk):  1.3333333333333333
+        Q95 over time:  [0. 8. 0.]
+        Objective 2 (expected excess):  1.3333333333333333
+```
+
+The first detected error does not interrupt the checking process. However, since the evaluated solution is not valid, the computed indicators are no more relevant.
+
+
+## Carried checks
+---
+Following checks are being carried by checker.  
+If none of the following erros has been uncovered, the solution is considered valid.
+
+Constraints mentioned by a paragraph number in error messages refer to the ones described in the challenge's presentation document.
+
+
+### Undefined intervention
+Solution file contains a reference to an intervention that does not exist in the instance.
+```
+ERROR: Unexpected Intervention I100 in solution file output1.txt.
+```
+Intervention is then ignored.
+
+
+### Starting date format unmet
+Intervention's starting date is not an integer.
+```
+ERROR: Unexpected starting time 1.0 for Intervention I1. Expect integer value.
+```
+Intervention is considered unscheduled, and is not taken into account in further checks and indicators computation.
+
+### Duplicate entry
+Solution file defines several starting date for a same intervention (i.e. intervention appears on several lines).
+```
+ERROR: Duplicate entry for Intervention I1. Only first read value is being considered.
+```
+Only the first date is taken into account.
+
+### Unscheduled intervention
+An intervention has not been scheduled: it is not present in the solution file, or its starting date is ineligible and was previously ignored.
+```
+ERROR: Schedule constraint 4.1.2: Intervention I1 has not been scheduled.
+```
+
+### Starting date out of planification range
+An intervention's starting date is out of the range of the planification.
+```
+ERROR: Schedule constraint 4.1 time validity: Intervention I1 starting time 4 is not a valid starting date. Expected value between 1 and 3.
+```
+Intervention is considered unscheduled, and is not taken into account in further checks and indicators computation.
+
+### Starting date is beyond interevntion's limit
+An intervention's starting date is posterior to the intervention's limit date, beyond which it cannot be scheduled in order to be finished in time (before the latest considered planification's period).
+```
+ERROR: Schedule constraint 4.1.3: Intervention I1 realization exceeds time limit. It starts at 2 while time limit is 1.
+```
+Intervention is considered unscheduled, and is not taken into account in further checks and indicators computation.
+
+### Workload exceeding resource upper bound
+Total workload over a period (arising from the interventions schedule) is greater than a resource availability.
+```
+ERROR: Resources constraint 4.2 upper bound: Workload on Resource c1 at time 0 exceeds upper bound. Value 50.0 is greater than bound 49 plus tolerance 1e-05.
+```
+This check is carried out with a 1e-5 tolerance.
+
+### Workload not meeing resource lower bound
+Total workload over a period (arising from the interventions schedule) does not met a resource necessary minimum usage.
+```
+ERROR: Resources constraint 4.2 lower bound: Workload on Resource c1 at time 0 does not match lower bound. Value 0.0 is lower than bound 10 minus tolerance 1e-05.
+```
+This check is carried out with a 1e-5 tolerance.
+
+### Unmet exclusion
+Two interventions that can not be done concurrently are yet performed on overlapping periods.
+```
+ERROR: Exclusions constraint 4.3: Interventions I2 and I3 are both ongoing at time 1.
+```
+
+
+
+## Remarks
+---
+**Remark 1:** no test ensures that the given solution does match the provided instance (no shared name does exist to ensure so).  
+It is up to the user to ensure a certain level of consistency between the supplied files.
+
+**Remark 2:** paths may be given as relative paths.  
+The reference for relative path is the current repository.
+
+**Remark 3:** the checker does not stop on the first found error. All possible erros are looked up for to try and be as comprehensible as possible in a single run.  
+Nontheless, if inconsistencies are detected on first constraint, the subsequent checks might be skewed if some information were to be ommited.
+For example, an intervention could have been considered unscheduled, and thus not been taken into account to compute workload.
+
+## Instances format
+
+The instance's data format is described in the challenge's rule document (§3.1.1).
+
+Two small functional examples are provided: `exemple1.json` and `exemple2.json`.
+
+### Commented example
+
+The items of an insance are elucidated below.  
+One can also look at the swagger model description to benefit from an alternative presentation of the underlying data model.
+
+#### Resources
+Set of available resources.  
+A particular resource is identified by its name.  
+The upper ("max") and lower ("min") bounds on resource availability are lists of float of length T (total duration of the instance).  
+```
+"Resources": {
+  "c1" : {
+    "max": [49.0, 23.0, 15.0],
+    "min": [10.0, 0.0, 6.0]
+}
+```
+
+#### Seasons
+Studied periods are split into seasons.  
+Seasons are used to define exclusions.  
+Possible seasons are:
+- "full": has to contain all periods
+- "winter"
+- "summer"
+- "is": inter-season
+
+Each period is present in one and only one season.
+```
+"Seasons": {
+  "winter": [1, 2],
+  "summer": [3],
+  "is": []
+}
+```
+
+#### Interventions
+Set of all interventions to schedule.  
+A particular intervention is identified by its name.
+
+"tmax" is the date/period before which the intervention has to be scheduled (if it is scheduled after that date, it will still be ongoing after the planification limit date, which is forbidden).
+
+"Delta" is the duration of the intervention depending on its starting date.  
+It is a list of size T (total duration of instance), though values of index beyond tmax are not used.
+
+Workload gives the the consumption of resources by the intervention (see below for details).
+
+Risk gives the risk values associated with the intervention realization (see below for details).
+
+
+```
+"Interventions": {
+  "I1": {
+    "tmax": 1,
+      "Delta": [3, 3, 2],
+      "workload": {...},
+      "risk": {...}
+  }
+```
+
+##### Workload
+Worlad describes the usage of each needed resource.  
+Used resources are identified by their names ("c1" here).  
+Several resources might be needed.  
+Resources hereby mentionned have to be defined in the Resources set.
+
+> "t" : { "st_1": w_1, "st_2": w_2, "st_3": w_3}
+
+For a given resource, the workload is given for each timestep "t".  
+For each timestep, the workload "w" is given depending on the starting time "st" of the intervention.
+```
+workload": {
+  "c1": {
+    "1": { "1": 14, "2":  0, "3":  0},
+    "2": { "1":  0, "2": 14, "3":  0},
+    "3": { "1":  0, "2":  0, "3": 14}
+  }
+}
+```
+
+**Remark:** Contrary to what is shown in the above example, null values are not written in generated instances. Some element might hence be "missing" in the table.
+
+
+##### Risk
+> "t": {"st_1":[...], "st_2":[...]}
+
+Risk values for each scnerio are given for each timestep "t", and depending on the intervention starting time "st".  
+For a given "t" and "st", risk is a list whose size is equal to the number of scenario (see below) for the current timestep. It gives a risk value for each of the scenario.
+```
+"risk": {
+  "1": {"1": [4, 8, 2], "2": [0, 0, 0]},
+  "2": {"1": [0, 0], "2": [3, 8]},
+  "3": {"1": [0, 0, 0], "2": [0, 0, 0]}
+sss}
+```
+
+#### Exclusions
+Set of interventions mutual exclusion over a season.  
+The interventions names and the season name are given in a list.  
+The interventions shall not be concurrently ongoing on any of the season's period.
+```
+"Exclusions": {
+  "E1": ["I2", "I3", "full"]
+}
+```
+
+#### Instance horizon
+The index of the last period belonging to the studied horizon.  
+As each study starts at time 1, it is also the length of the studied horizon.
+
+```
+"T": 3
+```
+
+#### Scenario number
+Number of available and considered scenario for each timestep.  
+It is a list of size T.
+```
+"Scenarios_number": [3, 2, 3]
+```
+
+## Solutions format
+
+The solution's data format is also described in the rule document (§3.1.2).
+
+Solutions matching the prodvided examples are also available: `output1.txt` and `output2.txt` respectively.
+
+**Remark:** beside all the hereby described cheks, the solution format is not being tested.  
+Format issues may arise to script failure.
---
+++ RTE_ChallengeROADEF2020_checker.py
@@ -0,0 +1,352 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  9 16:39:09 2019
+Last update on Wed Feb 12 10:35:00 2020
+
+@author: rte-challenge-roadef-2020-team
+"""
+import operator
+import os
+import sys
+import numpy as np
+import json
+
+
+####################
+### Utils ##########
+####################
+
+## Global variables
+CUR_DIR = os.getcwd()
+PAR_DIR = os.path.dirname(CUR_DIR)
+RESOURCES_STR = 'Resources'
+SEASONS_STR = 'Seasons'
+INTERVENTIONS_STR = 'Interventions'
+EXCLUSIONS_STR = 'Exclusions'
+T_STR = 'T'
+SCENARIO_NUMBER = 'Scenarios_number'
+RESOURCE_CHARGE_STR = 'workload'
+TMAX_STR = 'tmax'
+DELTA_STR = 'Delta'
+MAX_STR = 'max'
+MIN_STR = 'min'
+RISK_STR = 'risk'
+START_STR = 'start'
+
+## Json reader
+def read_json(filename: str):
+    """Read a json file and return data as a dict object"""
+
+    print('Reading json file ' + filename + '...')
+    f = open(filename, 'r')
+    Instance = json.load(f)
+    f.close()
+    print('Done')
+
+    return Instance
+
+## Txt Solution reader
+def read_solution_from_txt(Instance: dict, solution_filename: str):
+    """Read a txt formated Solution file, and store the solution informations in Instance"""
+
+    print('Loading solution from ' + solution_filename + '...')
+    # Load interventions
+    Interventions = Instance[INTERVENTIONS_STR]
+    # Read file line by line, and store starting time value (no checks yet, except format and duplicate)
+    solution_file = open(solution_filename, 'r')
+    for line in solution_file:
+        # Split line to retrive infos: Intervention name and decided starting date
+        tmp = line.split(' ')
+        intervention_name = tmp[0]
+        start_time_str = tmp[1].split('\n')[0]
+        # Assert Intervention exists
+        if not intervention_name in Interventions:
+            print('ERROR: Unexpected Intervention ' + intervention_name + ' in solution file ' + solution_filename + '.')
+            continue
+        # Assert starting date is an integer
+        start_time: int
+        try:
+            start_time = int(start_time_str)
+        except ValueError:
+            print('ERROR: Unexpected starting time ' + start_time_str + ' for Intervention ' + intervention_name + '. Expect integer value.')
+            continue
+        # Assert no duplicate
+        if START_STR in Interventions[intervention_name]:
+            print('ERROR: Duplicate entry for Intervention ' + intervention_name + '. Only first read value is being considered.')
+            continue
+        # Store starting time
+        Interventions[intervention_name][START_STR] = start_time
+    solution_file.close()
+    print('Done')
+
+
+################################
+## Results processing ##########
+################################
+
+## Compute effective worload
+def compute_resources(Instance: dict):
+    """Compute effective workload (i.e. resources consumption values) for every resource and every time step"""
+
+    # Retrieve usefull infos
+    Interventions = Instance[INTERVENTIONS_STR]
+    T_max = Instance[T_STR]
+    Resources = Instance[RESOURCES_STR]
+    # Init resource usage dictionnary for each resource and time
+    resources_usage = {}
+    for resource_name in Resources.keys():
+        resources_usage[resource_name] = np.zeros(T_max)
+    # Compute value for each resource and time step
+    for intervention_name, intervention in Interventions.items():
+        # start time should be defined (already checked in scheduled constraint checker)
+        if not START_STR in intervention:
+            continue
+        start_time = intervention[START_STR]
+        start_time_idx = start_time - 1 #index of list starts at 0
+        intervention_worload = intervention[RESOURCE_CHARGE_STR]
+        intervention_delta = int(intervention[DELTA_STR][start_time_idx])
+        # compute effective worload
+        for resource_name, intervention_resource_worload in intervention_worload.items():
+            for time in range(start_time_idx, start_time_idx + intervention_delta):
+                # null values are not available
+                if str(time+1) in intervention_resource_worload and str(start_time) in intervention_resource_worload[str(time+1)]:
+                    resources_usage[resource_name][time] += intervention_resource_worload[str(time+1)][str(start_time)]
+
+    return resources_usage
+
+
+## Retrieve effective risk distribution given starting times solution
+def compute_risk_distribution(Interventions: dict, T_max: int, scenario_numbers):
+    """Compute risk distributions for all time steps, given the interventions starting times"""
+
+    print('\tComputing risk...')
+    # Init risk table
+    risk = [scenario_numbers[t] * [0] for t in range(T_max)]
+    # Compute for each intervention indeoendently
+    for intervention in Interventions.values():
+        # Retrieve Intervention's usefull infos
+        intervention_risk = intervention[RISK_STR]
+        # start time should be defined (already checked in scheduled constraint checker)
+        if not START_STR in intervention:
+            continue
+        start_time = intervention[START_STR]
+        start_time_idx = int(start_time) - 1 # index for list getter
+        delta = int(intervention[DELTA_STR][start_time_idx])
+        for time in range(start_time_idx, start_time_idx + delta):
+            risk[time] = intervention_risk[str(time + 1)][str(start_time)]
+    print('\tDone')
+
+    return risk
+
+## Compute mean for each period
+def compute_mean_risk(risk, T_max: int, scenario_numbers):
+    """Compute mean risk values over each time period"""
+
+    print('\tComputing mean risk...')
+    # Init mean risk
+    mean_risk = np.zeros(T_max)
+    # compute mean
+    for t in range(T_max):
+        mean_risk[t] = sum(risk[t]) / scenario_numbers[t]
+    print('\tDone')
+
+    return mean_risk
+
+## Compute Q95 quantile for each period
+def compute_q95(risk, T_max: int, scenario_numbers):
+    """Compute Q95 values over each time period"""
+
+    print('\tComputing Q95...')
+    # Init q95
+    q95 = np.zeros(T_max)
+    for t in range(T_max):
+        risk[t].sort()
+        q95[t] = risk[t][int(np.floor(scenario_numbers[t] * 0.95))]
+    print('\tDone')
+
+    return q95
+
+## Compute both objectives: mean risk and q95
+def compute_objective(Instance: dict):
+    """Compute objectives (mean and expected excess)"""
+
+    print('Computing objectives values...')
+    # Retrieve usefull infos
+    T_max = Instance[T_STR]
+    scenario_numbers = Instance[SCENARIO_NUMBER]
+    Interventions = Instance[INTERVENTIONS_STR]
+    # Retrieve risk final distribution
+    risk = compute_risk_distribution(Interventions, T_max, scenario_numbers)
+    # Compute mean risk
+    mean_risk = compute_mean_risk(risk, T_max, scenario_numbers)
+    # Compute Q95 quantile
+    q95 = compute_q95(risk, T_max, scenario_numbers)
+    print('Done')
+
+    return mean_risk, q95
+
+
+
+##################################
+## Constraints checkers ##########
+##################################
+
+## Launch each Constraint checks
+def check_all_constraints(Instance: dict):
+    """Run all constraint checks"""
+
+    print('Checking constraints...')
+    # Schedule constraints
+    check_schedule(Instance)
+    # Resources constraints
+    check_resources(Instance)
+    # Exclusions constraints
+    check_exclusions(Instance)
+    print('Done')
+
+## Schedule constraints: §4.1 in model description
+def check_schedule(Instance: dict):
+    """Check schedule constraints"""
+
+    print('\tChecking schedule constraints...')
+    # Continuous interventions: §4.1.1
+    #   This constraint is implicitly checked by the resource computation:
+    #   computation is done under continuity hypothesis, and resource bounds will ensure the feasibility
+    # Checks is done on each Intervention
+    Interventions = Instance[INTERVENTIONS_STR]
+    for intervention_name, intervention in Interventions.items():
+        # Interventions are planned once: §4.1.2
+        #   assert a starting time has been assigned to the intervention
+        if not START_STR in intervention:
+            print('ERROR: Schedule constraint 4.1.2: Intervention ' + intervention_name + ' has not been scheduled.')
+            continue
+        # Starting time validity: no explicit constraint
+        start_time = intervention[START_STR]
+        horizon_end = Instance[T_STR]
+        if not (1 <= start_time <= horizon_end):
+            print('ERROR: Schedule constraint 4.1 time validity: Intervention ' + intervention_name + ' starting time ' + str(start_time)
+            + ' is not a valid starting date. Expected value between 1 and ' + str(horizon_end) + '.')
+            # Remove start time to avoid later access errors
+            del intervention[START_STR]
+            continue
+        # No work left: §4.1.3
+        #   assert intervention is not ongoing after time limit or end of horizon
+        time_limit = int(intervention[TMAX_STR])
+        if time_limit < start_time:
+            print('ERROR: Schedule constraint 4.1.3: Intervention ' + intervention_name + ' realization exceeds time limit.'
+            + ' It starts at ' + str(start_time) + ' while time limit is ' + str(time_limit) + '.')
+            # Remove start time to avoid later access errors
+            del intervention[START_STR]
+            continue
+    print('\tDone')
+
+## Resources constraints: §4.2 in model description
+def check_resources(Instance: dict):
+    """Check resources constraints"""
+
+    print('\tChecking resources constraints...')
+    T_max = Instance[T_STR]
+    Resources = Instance[RESOURCES_STR]
+    # Bounds are checked with a tolerance value
+    tolerance = 1e-5
+    # Compute resource usage
+    resource_usage = compute_resources(Instance) # dict on resources and time
+    # Compare bounds to usage
+    for resource_name, resource in Resources.items():
+        for time in range(T_max):
+            # retrieve bounds values
+            upper_bound = resource[MAX_STR][time]
+            lower_bound = resource[MIN_STR][time]
+            # Consumed value
+            worload = resource_usage[resource_name][time]
+            # Check max
+            if worload > upper_bound + tolerance:
+                print('ERROR: Resources constraint 4.2 upper bound: Worload on Resource ' + resource_name + ' at time ' + str(time) + ' exceeds upper bound.'
+                + ' Value ' + str(worload) + ' is greater than bound ' + str(upper_bound) + ' plus tolerance ' + str(tolerance) + '.')
+            # Check min
+            if worload < lower_bound - tolerance:
+                print('ERROR: Resources constraint 4.2 lower bound: Worload on Resource ' + resource_name + ' at time ' + str(time) + ' does not match lower bound.'
+                + ' Value ' + str(worload) + ' is lower than bound ' + str(lower_bound) + ' minus tolerance ' + str(tolerance) + '.')
+    print('\tDone')
+
+## Exclusions constraints: §4.3 in model description
+def check_exclusions(Instance: dict):
+    """Check exclusions constraints"""
+
+    print('\tChecking exclusions constraints...')
+    # Retrieve Interventions and Exclusions
+    Interventions = Instance[INTERVENTIONS_STR]
+    Exclusions = Instance[EXCLUSIONS_STR]
+    # Assert every exclusion holds
+    for exclusion in Exclusions.values():
+        # Retrieve exclusion infos
+        [intervention_1_name, intervention_2_name, season] = exclusion
+        # Retrieve concerned interventions...
+        intervention_1 = Interventions[intervention_1_name]
+        intervention_2 = Interventions[intervention_2_name]
+        # start time should be defined (already checked in scheduled constraint checker)
+        if (not START_STR in intervention_1) or (not START_STR in intervention_2):
+            continue
+        # ... their respective starting times...
+        intervention_1_start_time = intervention_1[START_STR]
+        intervention_2_start_time = intervention_2[START_STR]
+        # ... and their respective deltas (duration)
+        intervention_1_delta = int(intervention_1[DELTA_STR][intervention_1_start_time - 1]) # get index in list
+        intervention_2_delta = int(intervention_2[DELTA_STR][intervention_2_start_time - 1]) # get index in list
+        # Check overlaps for each time step of the season
+        for time_str in Instance[SEASONS_STR][season]:
+            time = int(time_str)
+            if (intervention_1_start_time <= time < intervention_1_start_time + intervention_1_delta) and (intervention_2_start_time <= time < intervention_2_start_time + intervention_2_delta):
+                print('ERROR: Exclusions constraint 4.3: Interventions ' + intervention_1_name + ' and ' + intervention_2_name
+                    + ' are both ongoing at time ' + str(time) + '.')
+    print('\tDone')
+
+
+#######################
+## Displayer ##########
+#######################
+
+## Basic printing
+def display_basic(Instance: dict, mean_risk, q95):
+    """Print main infos"""
+
+    # Infos about instance
+    print('Instance infos:')
+    print('\tInterventions number: ', len(Instance[INTERVENTIONS_STR]))
+    print('\tScenario numbers: ', len(Instance[SCENARIO_NUMBER]))
+    # Computed infos about solution
+    print('Solution evaluation:')
+    print('\tmean_risk over time: ', mean_risk)
+    print('\tObjective 1 (mean risk): ', np.mean(mean_risk))
+    print('\tQ95 over time: ', q95)
+    tmp = np.zeros(len(q95))
+    print('\tObjective 2 (expected excess): ', np.mean(np.max(np.vstack((q95 - mean_risk, tmp)), axis=0)))
+
+
+######################
+## Launcher ##########
+######################
+
+def check_and_display(instance_file, solution_file):
+    """Control checker actions"""
+
+    # Read Instance
+    instance = read_json(instance_file)
+    # Read Solution
+    read_solution_from_txt(instance, solution_file)
+    # Check all constraints
+    check_all_constraints(instance)
+    # Compute indicators
+    mean_risk, q95 = compute_objective(instance)
+    # Display Solution
+    display_basic(instance, mean_risk, q95)
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 3:
+        print('ERROR: expecting 2 arguments: paths to instance and solution files')
+    else:
+        instance_file = sys.argv[1]
+        solution_file = sys.argv[2]
+        check_and_display(instance_file, solution_file)
